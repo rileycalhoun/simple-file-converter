70ba73ad25a181a998f077b49f561b08cdc265e6
Riley Calhoun <rileycalhoun0228@gmail.com>
2024-07-18 20:25:22 -0700

    add a button for my wife

---
+++ src/lib/HomeButton.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { goto } from "$app/navigation";
+    import { base } from "$app/paths";
+    
+    function onClick() {
+        goto(base + "/")
+    }
+
+</script>
+
+<button on:click={onClick}>Home</button>
+
+<style>
+
+    button {
+        padding: 12px 25px;
+        border: none;
+        border-radius: 8px;
+        background-color: #007bff;
+        color: #fff;
+        font-size: 1.1em;
+        cursor: pointer;
+        transition: background-color 0.3s, transform 0.2s;
+        width: 99%;
+    }
+
+
+</style>
--- src/routes/files/[id]/+page.svelte
+++ src/routes/files/[id]/+page.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import type { PageData } from "./$types";
 	import Viewer from "$lib/Viewer.svelte";
+	import HomeButton from "$lib/HomeButton.svelte";
 
     export let data: PageData;
-    export let props: { file: { name: string, base64: string } } = data.props;
-    export let { file } = props;
+    let props: { file: { name: string, base64: string } } = data.props;
+    let { file } = props;
 </script>
 
 <Viewer
     base64={file.base64}
     downloadName={file.name} />
+<HomeButton />
